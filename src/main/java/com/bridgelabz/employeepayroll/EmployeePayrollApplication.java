487d8077d75e2a576c0163d9df29b4cd403de469
erakshaykumar <akaakshay304@gmail.com>
2022-05-07 10:32:23 +0530

    Added: Welcome Message To Employee PayRoll Application

---
+++ src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.employeepayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Employee PayRoll Application");
+		SpringApplication.run(EmployeePayrollApplication.class, args);
+	}
+
+}
