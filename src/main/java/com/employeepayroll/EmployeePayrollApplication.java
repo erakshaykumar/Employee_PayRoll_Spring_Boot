f35fe28a40a388e3ec521e02d5a4def0fd27eae7
erakshaykumar <akaakshay304@gmail.com>
2022-05-14 09:15:33 +0530

    Added: Setting Up Dev And Production Environment

--- src/main/java/com/employeepayroll/EmployeePayrollApplication.java
+++ src/main/java/com/employeepayroll/EmployeePayrollApplication.java
@@ -3,6 +3,8 @@ package com.employeepayroll;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
 /**
 Use Lombok Library for Logging i.e Slf4j
  */
@@ -12,8 +14,9 @@ public class EmployeePayrollApplication {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Payroll App !!!!!");
-        SpringApplication.run(EmployeePayrollApplication.class, args);
-        log.info("employee payroll app started");
+        ApplicationContext context = SpringApplication.run(EmployeePayrollApplication.class, args);
+        log.info("Employee Payroll App Started in {} Environment",
+                context.getEnvironment().getProperty("environment"));
     }
 
 }
