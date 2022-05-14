62fea9661bb99473baec3c6563d89970d61a7883
erakshaykumar <akaakshay304@gmail.com>
2022-05-14 10:43:59 +0530

    Added: Remaining Properties To DTO And Data

--- src/main/java/com/employeepayroll/dto/EmployeePayrollDTO.java
+++ src/main/java/com/employeepayroll/dto/EmployeePayrollDTO.java
@@ -1,23 +1,23 @@
 package com.employeepayroll.dto;
 
+import lombok.ToString;
+
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
+import java.time.LocalDate;
+import java.util.List;
 
-public class EmployeePayrollDTO {
+public @ToString class EmployeePayrollDTO {
     @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name is Invalid")
     @NotEmpty(message = "Employee name cannot be null")
     public String name;
     @Min(value = 500, message = "min wage should be more than 500")
-    public long salary;
+    public Long salary;
+    public String gender;
+    public LocalDate startDate;
+    public String note;
+    public String profilePic;
+    public List<String> department;
 
-    public EmployeePayrollDTO(String name, long salary) {
-        this.name = name;
-        this.salary = salary;
-    }
-
-    @Override
-    public String toString() {
-        return "name="+name+":salary="+salary;
-    }
 }
--- src/main/java/com/employeepayroll/model/EmployeePayrollData.java
+++ src/main/java/com/employeepayroll/model/EmployeePayrollData.java
@@ -2,40 +2,29 @@ package com.employeepayroll.model;
 
 import com.employeepayroll.dto.EmployeePayrollDTO;
 
-public class EmployeePayrollData {
-    private  Long Salary;
-    private  String name;
+import lombok.Data;
+import java.time.LocalDate;
+import java.util.List;
+
+public @Data class EmployeePayrollData {
     private int employeeID;
+    private String name;
+    private Long salary;
+    public String gender;
+    public LocalDate startDate;
+    public String note;
+    public String profilePic;
+    private  List<String> department;
 
     public EmployeePayrollData(int empId, EmployeePayrollDTO employeePayrollDTO) {
         this.employeeID = empId;
-        this.name = employeePayrollDTO.name ;
-        this.Salary= employeePayrollDTO.salary;
-    }
-    public EmployeePayrollData() {
-    }
+        this.name = employeePayrollDTO.name;
+        this.salary = employeePayrollDTO.salary;
+        this.gender = employeePayrollDTO.gender;
+        this.startDate = employeePayrollDTO.startDate;
+        this.note=employeePayrollDTO.note;
+        this.profilePic=employeePayrollDTO.profilePic;
+        this.department=employeePayrollDTO.department;
 
-    public Long getSalary() {
-        return Salary;
-    }
-
-    public void setSalary(Long salary) {
-        Salary = salary;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getEmployeeID() {
-        return employeeID;
-    }
-
-    public void setEmployeeID(int employeeID) {
-        this.employeeID = employeeID;
     }
 }
