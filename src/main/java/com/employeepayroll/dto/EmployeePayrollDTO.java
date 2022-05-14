ddcc6a6d757463c236eae188bc867c32dc0a99fe
erakshaykumar <akaakshay304@gmail.com>
2022-05-14 11:45:27 +0530

    Added: Validation For Properties And Exception Handler

--- src/main/java/com/employeepayroll/dto/EmployeePayrollDTO.java
+++ src/main/java/com/employeepayroll/dto/EmployeePayrollDTO.java
@@ -1,10 +1,9 @@
 package com.employeepayroll.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.ToString;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -13,11 +12,22 @@ public @ToString class EmployeePayrollDTO {
     @NotEmpty(message = "Employee name cannot be null")
     public String name;
     @Min(value = 500, message = "min wage should be more than 500")
-    public Long salary;
-    public String gender;
-    public LocalDate startDate;
-    public String note;
-    public String profilePic;
-    public List<String> department;
+    public long salary;
 
+    @JsonFormat(pattern = "dd MM yyyy")
+    @NotNull(message = "start date should not be empty")
+    @PastOrPresent(message = "start date should be past or today")
+    public LocalDate startDate;
+
+    @Pattern(regexp = "male|female", message = "Gender needs to be male or female")
+    public String gender;
+
+    @NotBlank(message = "note should not be empty")
+    public String note;
+
+    @NotBlank(message = "profile pic should not be empty")
+    public String profilePic;
+
+    @NotNull(message = "department should not be empty")
+    public List<String> department;
 }
